b3215ee8cc86c6aba373865fb5733e9a4338ffd9
Shaikh Rumman Fardeen <rummanfardeen4567@gmail.com>
2023-09-07 20:13:04 +0000

    ✨ Details modified

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shaikh Rumman Fardeen",
+  title: "MERN, NextJS, Blockchain, UIUX Designer",
+  email: "rummanfardeen4567@gmail.com",
+  gitHub: "srummanf",
+  instagram: "rumf.ar",
+  linkedIn: "srummanf",
   medium: "",
-  twitter: "microsoft",
+  twitter: "srummanf",
   youTube: "Code",
 };
 
